ebc5302cc67cf5a2308fd824825d96c295f7afb7
xavierfd6 <64189491+xavierfd6@users.noreply.github.com>
2024-01-21 20:57:46 +0800

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'info@yourwebsite.com'
+$email_from = 'https://king-custom-web-gamma.vercel.app/index.html'
 
 $email_subject = 'New Form Submission'
 
